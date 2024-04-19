d1e6afb872aea9fd1d81a22a3e768ff58e9058f0
Katie Ortiz <katrina.ortiz.673@my.csun.edu>
2024-04-18 22:29:35 -0700

    Removed unused code and html page

--- application/app.py
+++ application/app.py
@@ -1,3 +1,6 @@
+# add error handling and messages to tell the user that stuff was successful
+
+
 from flask import Flask, render_template, request, redirect
 import pymysql
 
@@ -92,63 +95,6 @@ def search():
         # Render the search form page
         return render_template('search.html')
 
-
-# @app.route('/search', methods=['GET', 'POST'])
-# def search():
-#     if request.method == 'POST':
-#         # Handle search form submission and database query here
-#         search_field = request.form['search_field']
-#         search_query = request.form['search_query']
-
-#         # Perform database query based on search query and field
-#         connection = pymysql.connect(**db_config)
-#         try:
-#             with connection.cursor() as cursor:
-#                 if search_field == 'Author':
-#                     # Search by author name
-#                     sql = "SELECT p.* FROM Publication p INNER JOIN Author a ON p.ID = a.Publication_id WHERE a.FirstName LIKE %s OR a.LastName LIKE %s"
-#                     cursor.execute(sql, ('%' + search_query + '%', '%' + search_query + '%'))
-#                 elif search_field == 'Keywords':
-#                     # Search by keywords
-#                     sql = "SELECT p.* FROM Publication p INNER JOIN Keywords k ON p.ID = k.Publication_Pub_ID WHERE k.Keyword LIKE %s"
-#                     cursor.execute(sql, ('%' + search_query + '%',))
-#                 else:
-#                     # Search by other fields in Publication table
-#                     sql = f"SELECT * FROM Publication WHERE {search_field} LIKE %s"
-#                     cursor.execute(sql, ('%' + search_query + '%',))
-#                 results = cursor.fetchall()
-#         finally:
-#             connection.close()  # Close database connection
-
-#         # Render search results page with the retrieved data
-#         return render_template('search_results.html', search_query=search_query, results=results)
-#     else:
-#         # Render the search form page
-#         return render_template('search.html')
-
-
-# Edit to search for author name, insitution - should list publications per author
-@app.route('/author/<int:author_id>')
-def author(author_id):
-    # Handle retrieving author's publications from the database
-    connection = pymysql.connect(**db_config)
-    try:
-        with connection.cursor() as cursor:
-            # Execute the SQL command to retrieve author's information and publications
-            sql_author_info = "SELECT * FROM Author WHERE ID = %s"
-            cursor.execute(sql_author_info, (author_id,))
-            author_info = cursor.fetchone()
-
-            sql_author_publications = "SELECT * FROM Publication WHERE ID IN (SELECT Publication_id FROM Author WHERE ID = %s)"
-            cursor.execute(sql_author_publications, (author_id,))
-            author_publications = cursor.fetchall()
-    finally:
-        connection.close()  # Close database connection
-
-    # Render author publications page with the retrieved data
-    return render_template('author.html', author_info=author_info, author_publications=author_publications)
-
-
 @app.route('/add_publication', methods=['GET', 'POST'])
 def add_publication():
     if request.method == 'POST':
@@ -170,7 +116,7 @@ def add_publication():
         finally:
             connection.close()  # Close database connection
 
-        return redirect('/')  # Redirect to the homepage after adding publication
+        return redirect('/homepage')  # Redirect to the homepage after adding publication
     else:
         return render_template('add_publication.html')  # Render the add publication form page
 
